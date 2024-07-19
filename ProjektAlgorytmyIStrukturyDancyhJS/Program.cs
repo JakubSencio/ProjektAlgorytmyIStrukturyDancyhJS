3599de6fb4a93287388348a57f0639ca74c02bec
Jacob <sencio.jakub@gmail.com>
2024-07-19 23:25:56 +0200

    Dodaj pliki projektów.

---
+++ ProjektAlgorytmyIStrukturyDancyhJS/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjektAlgorytmyIStrukturyDancyhJS
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            CubicFunction cubicFunction = new CubicFunction();
+            Console.Write("Podaj liczbę: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double result = cubicFunction.Calculate(x);
+            Console.WriteLine($"Wynik funkcji sześciennej dla {x} to {result}");
+        }
+    }
+}
+}
